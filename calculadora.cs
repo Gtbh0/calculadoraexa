4a06a6e3db6d68e4902d4a3e7ae8d129649ec4a1
Gtbh0 <99442906+Gtbh0@users.noreply.github.com>
2022-11-01 20:27:48 -0500

    Update calculadora.cs
    operaciones listas en calculadora

--- calculadora.cs
+++ calculadora.cs
@@ -1,8 +1,37 @@
-﻿using System;
+using System;
 
 public class Class1
 {
-	public Class1()
-	{
-	}
+
+    private int num1;
+    private int num2;
+
+    public Class1()
+    {
+
+    }
+
+    public global::System.Int32 num1 { get => num1; = value; }
+    public global::System.Int32 num1 { get => num1; = value; }
+
+    public int suma()
+    {
+        return num1 + num2;
+    }
+	
+	 public int resta()
+    {
+        return num1 - num2;
+    }
+	
+	 public int multiplicacion()
+    {
+        return num1 * num2;
+    }
+	
+	 public int division()
+    {
+        return num1 / num2;
+    }
+
 }
