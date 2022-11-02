e11a8586045ba06b24a7579aefb61651740c60ca
Usuario <Usuario@DESKTOP-84QB8MI>
2022-11-01 20:04:42 -0500

    cree el archivo suma

---
+++ suma.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Class1
+{
+
+    private int num1;
+    private int num2;
+
+    public Class1()
+    {
+
+    }
+
+    public global::System.Int32 num1 { get => num1; = value; }
+    public global::System.Int32 num1 { get => num1; = value; }
+
+    public int suma()
+    {
+        return num1 + num2;
+    }
+
+}
